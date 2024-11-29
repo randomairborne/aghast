5f18fde1a3e9cf27bcd4578be4aa1c9a2151e691
valkyrie_pilot <valk@randomairborne.dev>
2024-11-28 23:11:28 -0700

    Rip out SQL

--- migrations/20241126202538_user_channel_table.sql
+++
@@ -1,8 +0,0 @@
--- Add migration script here
-CREATE TABLE tickets (
-    dm INT UNIQUE NOT NULL,
-    thread INT UNIQUE NOT NULL
-) STRICT;
-
-CREATE INDEX ticket_dms ON tickets(dm);
-CREATE INDEX ticket_threads ON tickets(thread);
